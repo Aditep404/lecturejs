c40ce0bfc86028386928a768cf2b7b5f6d6239de
Aditep404 <s6419410020@sau.ac.th>
2025-02-25 12:30:33 +0700

    complete

--- js/study01.js
+++ js/study01.js
@@ -1,19 +1,17 @@
-console.log('ABC...')
-console.log("ABC...")
-console.log(123456)
-console.log(true)
+//data type in javascript
 
-
-//ตัวแปร
-var a = 10           //ตัวแปรค่าเปลี่ยนได้ สามารถเปลี่ยนค่าได้ แบบ Global ใช้ที่ไหนก็ได้
-let b = 20          //ตัวแปรค่าเปลี่ยนได้ สามารถเปลี่ยนค่าได้  แบบ Local ใช้ได้เฉพราะใน { } หนึ่งๆ เท่านั้น
-const c = 30        //ตัวแปรค่าไม่เปลี่ยน ไม่สามารถเปลี่ยนค่าได้ แบบ Local ใช้ได้เฉพราะใน { } หนึ่งๆ เท่านั้น
-
-
-let data01 = 'wow...'
-let data02 = '123456'
-
-// เวลาเอาข้อมูลหลากหลายแบบมาใข้ร่วมกัน
-console.log('AAA' + ' ' + 555 + ' ' + data01 + ' ' + data02)
-
-console.log(`AAA ${555} ${data01} ${data02}`)
+//String
+console.log("ABC..."); console.log('ABC')
+//Number
+console.log(123456) ;
+console.log(115.546)
+//Boolean
+console.log(true)  //false
+//Array
+console.log([1, 2, 3, 4, 5, 'Hello...', true])
+//Object
+console.log({fullname:'John', age:20})
+//Null
+console.log(null);
+//Undefined
+console.log(undefined)
